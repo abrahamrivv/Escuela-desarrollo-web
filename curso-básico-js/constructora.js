08f2fa6a1cce04c10a4aead1977f2df8433df010
Abraham Rivv <uke10gaimer@gmail.com>
2021-06-23 19:24:07 -0500

    Día 16 escuela desarrollo web :D

--- curso-básico-js/constructora.js
+++ curso-básico-js/constructora.js
@@ -13,3 +13,23 @@ var AutoNuevo3 = new auto("Mercedez", "alch no c", 2020);
 console.log(AutoNuevo1);
 console.log(AutoNuevo2);
 console.log(AutoNuevo3);
+
+// ejercicio generar con cilco for objetos de autos automáticos
+function auto (MARCA, MODELO, ANNIO){
+    this.marca = MARCA;
+    this.modelo = MODELO;
+    this.annio = ANNIO;
+  }
+  
+  let autos = [];
+  
+  for(let i = 0 ; i < 5 ; i++){
+    let MARCA = prompt("Ingresa la marca del auto:");
+    let MODELO = prompt("Ingresa el modelo del auto:");
+    let ANNIO = prompt("Ingresa el año del auto:");
+    autos.push(new auto (MARCA, MODELO, ANNIO));
+  }
+
+  for(let i = 0 ; i < autos.length ; i++){
+    console.log(autos[i]);
+  }
--- curso-básico-js/objects.js
+++ curso-básico-js/objects.js
@@ -3,7 +3,7 @@ var miAuto = {
     modelo: "Model 3",
     annio: 2020,
     detalleDelAuto: function(){
-        console.log('Auto',this.marca, this.modelo, 'del año', this.annio);
+        console.log(`Auto ${this.marca} ${this.modelo} del año ${this.annio}`);
     }//¿Quién es this? Es una variable que hace referencia al objeto. En este caso: this = miAuto
 };
 
