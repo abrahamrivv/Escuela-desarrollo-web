30b6e18b6f6c7efbe1ece116fe7f396ec591d619
Abraham Rivv <uke10gaimer@gmail.com>
2021-07-23 14:39:56 -0500

    día 26 escuela desarrollo web

---
+++ closures_y_scope-js/src/debug/index.js
@@ -0,0 +1,12 @@
+var a = "hello"
+
+function hello() {
+    let b = "hello world"
+    const c = "hello world!"
+    if (true) {
+        let d = "hello world!!"
+        debugger
+    }
+}
+
+hello()
